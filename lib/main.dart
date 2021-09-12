d55b7aa8418446e095c4ce87306d2f3ec52d462d
giovanaprates <giovanaalprates@gmail.com>
2021-09-12 03:24:28 -0300

    Stateless e Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,41 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador de pessoas", //parametro title
-    home: Stack(
+    home: Home()
+  )
+  );
+}
+
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int _people = 0; //o <_> é para indicar que não quer que a variárvel seja acessada de fora, apenas dentro da classe
+  String _infoText = "Pode entrar!";
+
+  void _changePeople(int delta){
+    setState(() { //Executa o código que está aqui dentro e logo em seguida manda atualizar a tela
+      _people += delta;
+
+      if(_people < 0){
+        _infoText = "É o quê?";
+      }
+      else if(_people < 10){
+        _infoText = "Pode entrar!";
+      }
+      else{
+        _infoText = "Lotado!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
         Image.asset(
           "images/restaurant.jpg",
@@ -14,7 +48,7 @@ void main() {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              "Pessoas: 0",
+              "Pessoas: $_people",
               style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold),
             ),
             Row(
@@ -24,26 +58,33 @@ void main() {
                   padding: EdgeInsets.all(10.0),
                   child: FlatButton(
                     child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                    onPressed: (){},
+                    onPressed: (){
+                      /*debugPrint("+1");*/
+                      /*_people = _people++;*/
+                      _changePeople(1);
+                    },
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(10.0),
                   child: FlatButton(
                     child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
-                    onPressed: (){},
+                    onPressed: (){
+                      /*debugPrint("-1");*/
+                      /*_people = _people--;*/
+                      _changePeople(-1);
+                    },
                   ),
                 ),
               ],
             ),
             Text(
-              "Pode entrar!",
+              _infoText,
               style: TextStyle(color: Colors.purple, fontStyle: FontStyle.italic, fontSize: 30.0),
             )
           ],
         )
       ],
-    )
-  )
-  );
+    );
+  }
 }
