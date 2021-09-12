5b913011760e80c301e618ed8dec5be1f531dee4
giovanaprates <giovanaalprates@gmail.com>
2021-09-12 03:01:25 -0300

    Image, Padding, Stack

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,44 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador de pessoas", //parametro title
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    home: Stack(
       children: <Widget>[
-        Text(
-          "Pessoas: 0",
-          style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold),
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
         ),
-        Row(
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlatButton(
-              child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
-              onPressed: (){},
+          children: <Widget>[
+            Text(
+              "Pessoas: 0",
+              style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold),
             ),
-            FlatButton(
-              child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
-              onPressed: (){},
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text("+1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                    onPressed: (){},
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text("-1", style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                    onPressed: (){},
+                  ),
+                ),
+              ],
             ),
+            Text(
+              "Pode entrar!",
+              style: TextStyle(color: Colors.purple, fontStyle: FontStyle.italic, fontSize: 30.0),
+            )
           ],
-        ),
-        Text(
-          "Pode entrar!",
-          style: TextStyle(color: Colors.purple, fontStyle: FontStyle.italic, fontSize: 30.0),
         )
       ],
     )
